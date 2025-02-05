4ff1532fee1db694cbe5b82f34933a0343c4b0dd
tinisto <tinisto@gmail.com>
2025-02-04 21:19:56 -0600

    Fix: Prevent Redeclaration of redirectToErrorPage Function

--- includes/functions/redirectToErrorPage.php
+++ includes/functions/redirectToErrorPage.php
@@ -1,11 +1,13 @@
 <?php
-function redirectToErrorPage($errorMessage, $file, $line) {
-    $errorDetails = [
-        'message' => $errorMessage,
-        'file' => $file,
-        'line' => $line
-    ];
-    header("Location: /error.php?error=" . urlencode(json_encode($errorDetails)));
-    exit();
+if (!function_exists('redirectToErrorPage')) {
+    function redirectToErrorPage($errorMessage, $file, $line)
+    {
+        $errorDetails = [
+            'message' => $errorMessage,
+            'file' => $file,
+            'line' => $line
+        ];
+        header("Location: /error.php?error=" . urlencode(json_encode($errorDetails)));
+        exit();
+    }
 }
-?>
--- pages/account/reset-password/reset-password.php
+++ pages/account/reset-password/reset-password.php
@@ -2,6 +2,7 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/common-components/check_under_construction.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/functions/email_functions.php';
+include $_SERVER["DOCUMENT_ROOT"] . "/includes/functions/redirectToErrorPage.php";
 
 
 // Initialize $token variable outside the block
